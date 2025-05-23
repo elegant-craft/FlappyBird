539ff2001ea39bb11e9e9c5b9d696022444ac822
Henry <elegant.craft@hotmail.com>
2025-05-23 14:51:53 +0800

    Initial Commit

---
+++ FlappyBirdGame/FlappyBirdGame.h
@@ -0,0 +1,18 @@
+//
+//  FlappyBirdGame.h
+//  FlappyBirdGame
+//
+//  Created by apple on 2025/5/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlappyBirdGame.
+FOUNDATION_EXPORT double FlappyBirdGameVersionNumber;
+
+//! Project version string for FlappyBirdGame.
+FOUNDATION_EXPORT const unsigned char FlappyBirdGameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlappyBirdGame/PublicHeader.h>
+
+
