13056e36d0e9a713e194b024a584384b6271d14e
Henry <elegant.craft@hotmail.com>
2025-06-06 16:24:15 +0800

    change name

---
+++ FlappyBirdGame/FlappyBirdGameView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  FlappyBird Watch App
+//
+//  Created by Moon Jongseek on 3/18/24.
+//
+
+import SwiftUI
+import SpriteKit
+
+public struct FlappyBirdGameView: View{
+    @Binding var isPresented: Bool
+    
+    public init(isPresented: Binding<Bool>) {
+        self._isPresented = isPresented
+    }
+    
+    public var body: some View {
+        GeometryReader { geometryProxy in
+            let scene = GameScene(size: geometryProxy.size)
+            SpriteView(scene: scene)
+            .onTapGesture {
+                scene.tapGesture()
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
--- FlappyBirdGameiOS/FlappyBirdGameView.swift
+++ FlappyBirdGameiOS/FlappyBirdGameView.swift
@@ -8,16 +8,19 @@
 import SwiftUI
 import SpriteKit
 
-public struct ContentView3: View{
+public struct FlappyBirdGameView: View{
     @Binding var isPresented: Bool
+    @State var screenSize: CGSize
     
-    public init(isPresented: Binding<Bool>) {
+    public init(isPresented: Binding<Bool>, screenSize: CGSize) {
         self._isPresented = isPresented
+        self.screenSize = screenSize
     }
     
     public var body: some View {
         GeometryReader { geometryProxy in
-            let scene = GameScene(size: geometryProxy.size)
+//            let scene = GameScene(size: geometryProxy.size)
+            let scene = GameScene(size: CGSize(width: screenSize.width / 2, height: screenSize.height / 2))
             SpriteView(scene: scene)
             .onTapGesture {
                 scene.tapGesture()
